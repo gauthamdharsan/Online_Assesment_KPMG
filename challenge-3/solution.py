0c1a7042eb9e918bedf597a553d632c1dd562d08
gauthamdharsan <56825835+gauthamdharsan@users.noreply.github.com>
2022-08-18 21:00:21 +0530

    Update and rename challenge3.py to solution.py

--- challenge-3/solution.py
+++ challenge-3/solution.py
@@ -1,13 +1,3 @@
-# We have a nested object, we would like a function that you pass in the object and a key and get back the value.
-##
-# Example Inputs
-# object = {“a”:{“b”:{“c”:”d”}}}
-# key = a/b/c
-##
-# object = {“x”:{“y”:{“z”:”a”}}}
-# key = x/y/z
-# value = a
-
 def getKey(obj: dict):
     keys = list(obj)
     if len(keys) != 1:
